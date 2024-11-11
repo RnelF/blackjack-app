706c322b3ab492e403cd43d25c7152436682e34c
Arnel Francia <nelffff@gmail.com>
2024-11-11 19:40:43 +0800

    updated

--- app/components/handDisplay.tsx
+++ app/components/handDisplay.tsx
@@ -1,23 +1,3 @@
-import React from "react";
-import { ICard, getStrHand } from "../utils/blackjack";
-
-interface HandDisplayProps {
-  title: string;
-  hand: ICard[];
-  hideSecondCard?: boolean;
+export default function HandDisplay() {
+  return <div>Hello!</div>;
 }
-
-const HandDisplay: React.FC<HandDisplayProps> = ({
-  title,
-  hand,
-  hideSecondCard = false,
-}) => {
-  return (
-    <div className="hand-display">
-      <h2>{title}</h2>
-      <p>{getStrHand(hand, hideSecondCard)}</p>
-    </div>
-  );
-};
-
-export default HandDisplay;
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,21 @@
+"use client";
+import { useState } from "react";
+import HandDisplay from "./components/handDisplay";
 export default function Home() {
-  return <div></div>;
+  const [decision, setDecision] = useState("");
+  function getDecision(): "hit" | "stand" {
+    while (true) {
+      if (decision === "stand" || decision === "hit") return decision;
+    }
+  }
+  return (
+    <div>
+      <div className="flex flex-col justify-center items-center my-28">
+        <h1 className="text-5xl font-serif text-">Blackjack</h1>
+        <div className="my-10">
+          <HandDisplay />
+        </div>
+      </div>
+    </div>
+  );
 }
