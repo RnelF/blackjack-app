ad45df23f5782bd13c299c1b55d5b65c810ae5fc
Arnel Francia <nelffff@gmail.com>
2024-11-21 21:04:27 +0800

    restructuring components phase 4

---
+++ app/components/input-bet/Betting.tsx
@@ -0,0 +1,125 @@
+import { ICard } from "@/app/types";
+import Deck from "@/app/deck";
+import { getHandValue } from "@/app/utils";
+
+interface BettingProps {
+  bet: any;
+  balance: number;
+  deckQuantity: number;
+  deck: Deck;
+  play: boolean;
+  betError: string;
+  setBet: React.Dispatch<React.SetStateAction<any>>;
+  setBetError: React.Dispatch<React.SetStateAction<string>>;
+  setBalance: React.Dispatch<React.SetStateAction<number>>;
+  setDealerHand: React.Dispatch<React.SetStateAction<ICard[]>>;
+  setPlayerHand: React.Dispatch<React.SetStateAction<ICard[]>>;
+  setDeckQuantity: React.Dispatch<React.SetStateAction<number>>;
+  setDecision: React.Dispatch<React.SetStateAction<string>>;
+  setPlay: React.Dispatch<React.SetStateAction<boolean>>;
+  setBust: React.Dispatch<React.SetStateAction<boolean>>;
+  setGameDecision: React.Dispatch<React.SetStateAction<JSX.Element | string>>;
+}
+
+export default function Betting({
+  setBet,
+  setBetError,
+  bet,
+  balance,
+  setBalance,
+  setPlayerHand,
+  setDealerHand,
+  deck,
+  deckQuantity,
+  setDeckQuantity,
+  setDecision,
+  setBust,
+  setPlay,
+  setGameDecision,
+  play,
+  betError,
+}: BettingProps) {
+  function handleInputBet(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    if (value === "") {
+      setBet(0);
+      return;
+    }
+
+    // Otherwise, parse the input as a number
+    const parsedValue = parseFloat(value);
+
+    // Only update bet if it's a valid number
+    if (!isNaN(parsedValue)) {
+      setBet(parsedValue);
+      setBetError("");
+    }
+  }
+
+  function handleSubmitBet() {
+    const betAmount = parseFloat(bet);
+
+    // Validate the bet
+    if (betAmount > balance) {
+      setBetError("Insufficient Balance");
+      return;
+    } else if (isNaN(betAmount) || betAmount <= 0) {
+      setBetError("Invalid Bet!");
+      return;
+    }
+
+    // Deduct bet from balance and clear errors
+    setBalance(balance - betAmount);
+    setBetError("");
+
+    // Reset deck if quantity is low
+    if (deckQuantity < 10) {
+      deck.reset();
+    }
+
+    // Deal initial hands
+    const playerStartingHand = deck.deal(2);
+    const dealerStartingHand = deck.deal(2);
+    setPlayerHand(playerStartingHand);
+    setDealerHand(dealerStartingHand);
+    setDeckQuantity(deck.getDeckQuantity());
+
+    // Clear game state for a new round
+    setGameDecision("");
+    setDecision("");
+    setBust(false);
+    setPlay(true);
+
+    // Check for immediate Blackjack for player
+    const playerHandValue = getHandValue(playerStartingHand);
+    if (playerHandValue === 21) {
+      setDecision("stand");
+      setGameDecision("Blackjack! You win!");
+      setBalance((prevBalance) => prevBalance + betAmount * 2.5);
+      setPlay(false);
+    }
+  }
+
+  return (
+    <>
+      <div>
+        <div className={play ? "hidden" : ""}>
+          <input
+            type="text"
+            onChange={handleInputBet}
+            disabled={play}
+            value={bet}
+          />
+          <button
+            onClick={handleSubmitBet}
+            disabled={play}
+            className="border border-black bg-slate-50 rounded-md w-24 ml-3 font-semibold"
+          >
+            Place Bet
+          </button>
+          <div>{betError}</div>
+        </div>
+      </div>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,9 @@
 import DeckQuantity from "./components/deck-quantity/DeckQuantity";
 import GameButtons from "./components/game-buttons/GameButtons";
 import Hands from "./components/hands/Hands";
+import Betting from "./components/input-bet/Betting";
 import Deck from "./deck";
 import { ICard } from "./types";
-import { getHandValue, getStrHand } from "./utils";
 import { useState } from "react";
 
 export default function Home() {
@@ -19,67 +19,6 @@ export default function Home() {
   const [deck, setDeck] = useState(new Deck());
   const [play, setPlay] = useState(false);
   const [deckQuantity, setDeckQuantity] = useState(deck.getDeckQuantity());
-
-  function handleInputBet(event: React.ChangeEvent<HTMLInputElement>) {
-    const value = event.target.value;
-    if (value === "") {
-      setBet(0);
-      return;
-    }
-
-    // Otherwise, parse the input as a number
-    const parsedValue = parseFloat(value);
-
-    // Only update bet if it's a valid number
-    if (!isNaN(parsedValue)) {
-      setBet(parsedValue);
-      setBetError("");
-    }
-  }
-
-  function handleSubmitBet() {
-    const betAmount = parseFloat(bet);
-
-    // Validate the bet
-    if (betAmount > balance) {
-      setBetError("Insufficient Balance");
-      return;
-    } else if (isNaN(betAmount) || betAmount <= 0) {
-      setBetError("Invalid Bet!");
-      return;
-    }
-
-    // Deduct bet from balance and clear errors
-    setBalance(balance - betAmount);
-    setBetError("");
-
-    // Reset deck if quantity is low
-    if (deckQuantity < 10) {
-      deck.reset();
-    }
-
-    // Deal initial hands
-    const playerStartingHand = deck.deal(2);
-    const dealerStartingHand = deck.deal(2);
-    setPlayerHand(playerStartingHand);
-    setDealerHand(dealerStartingHand);
-    setDeckQuantity(deck.getDeckQuantity());
-
-    // Clear game state for a new round
-    setGameDecision("");
-    setDecision("");
-    setBust(false);
-    setPlay(true);
-
-    // Check for immediate Blackjack for player
-    const playerHandValue = getHandValue(playerStartingHand);
-    if (playerHandValue === 21) {
-      setDecision("stand");
-      setGameDecision("Blackjack! You win!");
-      setBalance((prevBalance) => prevBalance + betAmount * 2.5);
-      setPlay(false);
-    }
-  }
 
   return (
     <div className="flex flex-col gap-4 items-center justify-center">
@@ -124,22 +63,24 @@ export default function Home() {
         setDeckQuantity={setDeckQuantity}
       />
 
-      <div className={play ? "hidden" : ""}>
-        <input
-          type="text"
-          onChange={handleInputBet}
-          disabled={play}
-          value={bet}
-        />
-        <button
-          onClick={handleSubmitBet}
-          disabled={play}
-          className="border border-black bg-slate-50 rounded-md w-24 ml-3 font-semibold"
-        >
-          Place Bet
-        </button>
-        <div>{betError}</div>
-      </div>
+      <Betting
+        play={play}
+        deck={deck}
+        balance={balance}
+        bet={bet}
+        betError={betError}
+        deckQuantity={deckQuantity}
+        setDealerHand={setDealerHand}
+        setPlayerHand={setPlayerHand}
+        setDecision={setDecision}
+        setPlay={setPlay}
+        setBet={setBet}
+        setBetError={setBetError}
+        setGameDecision={setGameDecision}
+        setBalance={setBalance}
+        setBust={setBust}
+        setDeckQuantity={setDeckQuantity}
+      />
       <div>
         <p>Current Balance: {balance}</p>
       </div>
